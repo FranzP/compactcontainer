bdbf15e605e098e113da09831355204aa169080e
Germ?n Schuager <gschuager@gmail.com>
2010-09-27 22:43:19 -0300

    Added more AllTypes registration tests

--- src/CompactContainer.Tests/AllTypesRegistrationTests.cs
+++ src/CompactContainer.Tests/AllTypesRegistrationTests.cs
@@ -17,15 +17,48 @@ namespace CompactContainer.Tests
 		}
 
 		[Test]
-		public void Can_register_several_types_using_AllTypes_registration()
+		public void AllTypes_registration_should_register_BasedOn_asignable_types()
 		{
 			container.Register(
 				AllTypes.FromAssembly(Assembly.GetExecutingAssembly())
-					.BasedOn<IView>().WithService(SelectService.FirstInterface)
+					.BasedOn<IView>().WithService(x => x)
 				);
 
-			var firstComponentInfo = container.Components.FindServiceType(typeof (IView));
-			firstComponentInfo.Classtype.Should().Be.EqualTo(typeof(FirstView));
+			container.Components.Satisfy(components => components.Any(c => c.Classtype == typeof(FirstView)));
+		}
+
+		[Test]
+		public void AllTypes_registration_should_register_types_that_satisfy_Where_condition()
+		{
+			container.Register(
+				AllTypes.FromAssembly(Assembly.GetExecutingAssembly())
+					.Where(t => t.Name.EndsWith("View")).WithService(x => x)
+				);
+
+			container.Components.Satisfy(components => components.Any(c => c.Classtype == typeof(FirstView)));
+		}
+
+		[Test]
+		public void AllTypes_registration_should_allow_configuration_per_component()
+		{
+			container.Register(
+				AllTypes.FromAssembly(Assembly.GetExecutingAssembly())
+					.BasedOn<IView>().WithService(x => x).Configure(cfg => cfg.WithLifestyle(LifestyleType.Transient))
+				);
+
+			var c1 = container.Components.FindServiceType(typeof (FirstView));
+			c1.Lifestyle.Should().Be.EqualTo(LifestyleType.Transient);
+		}
+
+		[Test]
+		public void AllTypes_registration_should_allow_specification_of_registered_service()
+		{
+			container.Register(
+				AllTypes.FromAssembly(Assembly.GetExecutingAssembly())
+					.BasedOn<IView>().WithService(x => typeof(IView))
+				);
+
+			container.Components.Count(c => c.ServiceTypes.Contains(typeof(IView))).Should().Be.Equals(2);
 		}
 
 		public interface IView { }
